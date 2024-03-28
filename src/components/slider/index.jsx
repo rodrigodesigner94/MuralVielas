272993fbf6ba05c117e873c2fb8358f3a52b1ae6
Rodrigo Borges <digodesign94@gmail.com>
2024-03-27 21:44:35 -0300

    inserindo caixa de texto e ajuste da posição do carrossel de imagens

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -10,6 +10,15 @@ function Slider(){
         {id: '4', image: 'src/assets/imagens/52098983475_b0ffb9db6e_c.jpg'},
     ]
     return(
+        <div className='containerCarousel'>
+            <div className='txtbox'>
+                <h1 className='titleVielas'>VIELAS</h1>
+                <p className='txtVielas'>Um bando de imagens pensado para mostar
+                    a diversidade das periferias de São Paulo.
+                    Nascido nas quebracas e fomentado por quem
+                    vive e pensa nelas.
+                </p>
+            </div>
         <div className='container'>            
             <Swiper
             slidesPreview={1}           
@@ -19,12 +28,13 @@ function Slider(){
                     <SwiperSlide key={item.id}>
                         <img
                         src={item.image}
-                        className="slide-item"
+                        className='slideItem'
                         />
             
                     </SwiperSlide>
                 ))}
             </Swiper>
+        </div>
         </div>
     )
 }
